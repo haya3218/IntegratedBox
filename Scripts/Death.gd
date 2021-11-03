51ab235a1728ce6ccfdbff4c287a3c617721e9ae
/nick haya <74699483+The-SGPT@users.noreply.github.com>
2021-11-03 09:09:26 +0800

    add new death sound + fix crash for it

--- Scripts/Death.gd
+++ Scripts/Death.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+var CorrectSound = preload("res://Sounds/death.wav")
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -11,7 +12,11 @@ func _physics_process(delta):
 	for body in bodies:
 		if body.name == 'Player':
 			if not globals.player_died:
-				$DeathSound.play()
+				var deathsound = AudioStreamPlayer2D.new()
+				add_child(deathsound)
+				deathsound.max_distance = 99999
+				deathsound.stream = CorrectSound
+				deathsound.play()
 			globals.player_died = true
 			var left = Vector2(-2, 0)
 			Transitions.slide_rect2(self, '', 2, Color.black, left)
